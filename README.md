05ba5ba2e54daf5acad8455e31d7dee00fa915d2
Luc Russell <luc.russel@sproutee.com>
2018-01-30 16:26:23 -0500

    Updates for Prometheus

--- README.md
+++ README.md
@@ -1,28 +1,43 @@
 # Kafka Slack Bot
 
-A Slack bot for getting graphs and other data from Kafka.
+A simple Slack chatops bot, further writeup [here](TODO).
 
-## Set Up
-To build and run:
+## Prerequisites
+- Python 3.6+
+- [Docker](https://www.docker.com/)
+- [Kafkacat](https://github.com/edenhill/kafkacat)
 
-```
-$ docker-compose up -d
-```
+## Environment Setup
+There are several ways to set up a Python virtual environment, one is with pyenv and pip:
 
-This assumes entries for Kafka and Zookeeper in `/etc/hosts`, i.e.:
+    pyenv install 3.6.3
+    pyenv shell 3.6.3
+    mkvirtualenv --python=`pyenv which python` mynewenv
+    pip install -r requirements.txt
 
-```
-127.0.0.1 kafka
-127.0.0.1 zookeeper
-```
+Set up local networking to Kafka/Zookeeper with the following lines in `/etc/hosts`:
+
+    127.0.0.1 kafka
+    127.0.0.1 zookeeper
 
 ## Usage
-Request application metrics, e.g.: 
- 
-```
-@little_bot help me!
-@little_bot what is the lag like today?
-@little_bot graph it for me, please
-```
+Start the supporting services:
 
+    docker-compose up -d
+
+Send some messages:
+
+    for i in `seq 1 3`;
+    do
+            echo "{\"from\": \"alice\", \"to\":\"bob\", \"amount\": 3}"  | kafkacat -b kafka:9092 -t transactions -p 0
+    done
+
+Request application metrics, e.g.:
+ 
+
+    @little_bot help me!
+    @little_bot please show me the Kafka graphs
+    @little_bot restart 1
+
+(where `little_bot` is the name you chose when setting up the Slack bot).
 
--- slackbot/bot.py
+++ slackbot/bot.py
@@ -1,34 +1,56 @@
-import sys
-import shutil
+"""
 
+Usage:
+    slackbot [options]
+
+Options:
+    --config-file=<file>        Config file path.
+"""
 import io
-import requests
-
+import os
+import sys
 import time
-from slackclient import SlackClient
 
-from slackbot import lag
-from slackbot.lag import format_response
+import docker
+import yaml
+from docopt import docopt
+from slackclient import SlackClient
 
 
 class SlackBot(object):
 
     def __init__(self, config=None):
         self.config = config
-        self.token = config.get('slack_token')
-        self.bot_name = config.get('bot_name')
-        self.respond_to = ['lag', 'graphs', 'help']
+        self.token = self.config.get('slack_token')
+        self.slack_client = SlackClient(self.token)
+        self.bot_id = self.get_bot_id()
 
+        self.respond_to = ['list graph shortcuts',
+                           'graph <shortcut>',
+                           'help']
         self.help_msg = '```\n'
         for answer in self.respond_to:
-            self.help_msg += '{}\n'.format(answer)
+            self.help_msg += f'{answer}\n'
         self.help_msg += '```'
 
+        # This is a set of predefined shortcuts for commonly used graphs
         self.graph_urls = {}
-        for key, value in config['broker_groups'].items():
-            for name, url in value['graphs'].items():
-                self.graph_urls[name] = url
-        self.slack_client = SlackClient(self.token)
+        self.graph_shortcuts = '```\n'
+        for name, url in config['graph_urls'].items():
+            self.graph_urls[name] = url
+            self.graph_shortcuts += f'{name}\n'
+        self.graph_shortcuts += '```'
+
+    def get_bot_id(self):
+        api_call = self.slack_client.api_call("users.list")
+        if api_call.get('ok'):
+            # List all users so we can find our bot
+            users = api_call.get('members')
+            for user in users:
+                if 'name' in user and user.get('name') == self.config.get('bot_name'):
+                    return "<@" + user.get('id') + ">"
+
+            return None
 
     def start(self):
         if self.slack_client.rtm_connect():
@@ -37,70 +59,122 @@ class SlackBot(object):
                 events = self.slack_client.rtm_read()
                 for event in events:
                     if event.get('type') == 'message':
-                        response = self.on_message(event)
-                        if response:
-                            channel = event['channel']
-                            self.respond(channel, response)
+                        # If we received a message, read it and respond if necessary
+                        self.on_message(event)
+
                 time.sleep(1)
         else:
             print('Connection failed, invalid token?')
             sys.exit(1)
 
-    def on_message(self, msg):
-        if msg.get('subtype', '') == u'message_changed':
+    def on_message(self, event):
+        # Ignore edits and uploads
+        subtype = event.get('subtype', '')
+        if subtype == u'message_changed' or subtype == u'file_share':
             return
 
-        bot_name = "<@{}>".format(self.bot_name)
-
-        if msg.get('user', '') == self.bot_name:
+        # Don't respond to messages sent by the bot itself
+        if event.get('user', '') == self.bot_id:
             return
 
-        full_text = msg.get('text', '') or ''
-        if full_text.startswith(bot_name):
-            question = full_text[len(bot_name):]
+        full_text = event.get('text', '') or ''
+
+        # Only respond to messages addressed directly to the bot
+        if full_text.startswith(self.bot_id):
+            # Strip off the bot id and parse the rest of the message as the question
+            question = full_text[len(self.bot_id):]
             if len(question) > 0:
-                question = question.lower()
-                if 'lag' in question:
-                    lag_responder = lag.KafkaLag(self.config)
-                    try:
-                        return format_response(lag_responder.report())
-                    except Exception:
-                        return 'Hmm, I\'m having trouble connecting to Kafka'
-
+                question = question.strip().lower()
+                channel = event['channel']
+                if 'list graph shortcuts' in question:
+                    self.respond(channel, f'Graph shortcuts: {self.graph_shortcuts}')
                 elif 'graph' in question:
-                    return 'Generating graphs...'
-
+                    self.respond(channel, 'Please wait...', True)
                 elif 'help' in question:
-                    return 'I can answer questions about: {}'.format(
-                        self.help_msg)
+                    self.respond(channel, f'I can answer questions about: {self.help_msg}')
                 else:
-                    return 'Sorry, I can only answer ' \
-                           'questions about: {}'.format(self.help_msg)
+                    self.respond(channel, f'Sorry, I can only answer questions about: {self.help_msg}')
 
-    def respond(self, channel, text):
+    def respond(self, channel, text, upload=False):
         self.slack_client.api_call(
             'chat.postMessage',
             channel=channel,
             text=text,
             as_user='true:')
-        if 'graph' in text and 'help' not in text:
+        if upload:
             for graph_name, url in self.graph_urls.items():
-                self.upload_file(graph_name, url, channel)
+                self.generate_and_upload_graph(graph_name, url, channel)
 
-    def upload_file(self, filename, url, channel):
-        url_response = requests.get(url, stream=True)
-        with open(filename, 'wb') as out_file:
-            shutil.copyfileobj(url_response.raw, out_file)
-        del url_response
-        try:
-            with open(filename, 'rb') as in_file:
-                ret = self.slack_client.api_call(
-                    "files.upload",
-                    filename=filename,
-                    channels=channel,
-                    title=filename,
-                    file=io.BytesIO(in_file.read()))
-                if 'ok' not in ret or not ret['ok']:
-                    print('File upload failed %s', ret['error'])
-        except Exception as e:
-            print(e)
+    def generate_and_upload_graph(self, filename, url, channel):
+        # Create the graph in the current directory
+        dir_name = os.path.dirname(os.path.abspath(__file__))
+
+        client = docker.APIClient()
+
+        # Use the Docker Python API to dynamically create a container based on the alekzonder/puppeteer image
+        # Bind to the current directory so we can write the file somewhere accessible
+        # Use network_mode=host so the container can access Grafana over localhost
+        container = client.create_container(
+            image='alekzonder/puppeteer:1.0.0',
+            command=f'screenshot \'{url}\' 1366x768',
+            volumes=[dir_name],
+
+            host_config=client.create_host_config(binds={
+                dir_name: {
+                    'bind': '/screenshots'
+                }
+            }, network_mode='host')
+        )
+
+        files1 = prepare_dir(dir_name)
+
+        client.start(container)
+
+        # Poll for new files
+        while True:
+            time.sleep(2)
+            files2 = os.listdir(dir_name)
+            new = [f for f in files2 if all([f not in files1, f.endswith(".png")])]
+            for f in new:
+                with open(f, 'rb') as in_file:
+                    ret = self.slack_client.api_call(
+                        "files.upload",
+                        filename=filename,
+                        channels=channel,
+                        title=filename,
+                        file=io.BytesIO(in_file.read()))
+                    if 'ok' not in ret or not ret['ok']:
+                        print('File upload failed %s', ret['error'])
+                os.remove(f)
+            break
+
+
+def prepare_dir(dir_name):
+    # Check for any images from a previous run and remove them
+    files1 = os.listdir(dir_name)
+    for item in files1:
+        if item.endswith(".png"):
+            os.remove(os.path.join(dir_name, item))
+    return os.listdir(dir_name)
+
+
+def configure(filename):
+    if os.path.exists(filename) is False:
+        raise IOError("{0} does not exist".format(filename))
+
+    with open(filename) as config_file:
+        config_data = yaml.load(config_file)
+
+    return config_data
+
+
+def main(arguments=None):
+    if not arguments:
+        arguments = docopt(__doc__)
+    config = configure(arguments['--config-file'])
+    mybot = SlackBot(config)
+    mybot.start()
+
+
+if __name__ == "__main__":
+    main()
